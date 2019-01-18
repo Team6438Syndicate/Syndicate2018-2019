9b3c44768d8aec19250a454ab5710c3d2c0c3565
mattbatki <hugefacebook16@gmail.com>
2019-01-17 21:02:58 -0500

    Work plz

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunItNowDepot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunItNowDepot.java
@@ -19,7 +19,7 @@ import java.util.List;
 // Integrate way to control linearSlide in OpMode
 //Distance from landing to gems: Approximately 34 inches
 //Height of bracket off the ground: 19 inches
-
+//test
 @Autonomous(name = "Run It Now Depot", group = "Blue Autonomous 6438")
 public class RunItNowDepot extends LinearOpMode {
     //Reference to our hardware map
