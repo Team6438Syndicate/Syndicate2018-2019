cbda857ea671b97b57031fd4eb28387e64240e6a
mattbatki <hugefacebook16@gmail.com>
2019-03-12 14:51:04 -0400

    Major Code Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousHype.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousHype.java
@@ -201,8 +201,8 @@ public class AutonomousHype extends LinearOpMode
      *
      *  @param: Speed, inches for left and right
      */
-    //Direction needs to be either 1 for counterclockwise or -1 for clockwise "https://stemrobotics.cs.pdx.edu/node/7265"
-    private void gyroRobotTurn(double speed, int direction, double degrees)
+    //@condition Direction needs to be either 1 for counterclockwise or -1 for clockwise "https://stemrobotics.cs.pdx.edu/node/7265"
+    private void gyroRobotTurn(double speed, double degrees)
 {
     //Declaring new targets
     int leftTarget, rightTarget;
@@ -524,45 +524,4 @@ public class AutonomousHype extends LinearOpMode
         //creates a new parameters object
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
 
-        //Sets the minimum confidence for the program to read a block to the values stored in the Team6438HardwareMap
-        tfodParameters.minimumConfidence = robot.confidence;
-
-        //sets the tfod object equal to a new tfod object with parameters
-        robot.tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, robot.vuforia);
-
-        //Loads this years assets
-        robot.tfod.loadModelFromAsset(robot.TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, robot.LABEL_SILVER_MINERAL);
-    }
-
-    private void resetAngle()
-    {
-        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-
-        globalAngle = 0;
-    }
-
-    private double getAngle()
-    {
-        // We experimentally determined the Z axis is the axis we want to use for heading angle.
-        // We have to process the angle because the imu works in euler angles so the Z axis is
-        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
-        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
-
-        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-
-        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
-
-        if (deltaAngle < -180)
-            deltaAngle += 360;
-        else if (deltaAngle > 180)
-            deltaAngle -= 360;
-
-        globalAngle += deltaAngle;
-
-        lastAngles = angles;
-
-        return globalAngle;
-    }
-
-    //End of class
-}
+        //Sets the minimum confidence for the progra
