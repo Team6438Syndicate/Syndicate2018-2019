c9e869897c9b5ec74a6bc0e8206844de2548ffe1
Matthew <mkaboolian@gmail.com>
2019-03-21 10:36:36 -0400

    working on map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coordinateMapMaker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coordinateMapMaker.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 /*
  * test for the map of the field
@@ -9,9 +13,25 @@ import java.util.ArrayList;
  * Author: Matthew Kaboolian
  *
  */
-public class coordinateMapMaker
+public class coordinateMapMaker extends LinearOpMode
 {
    ArrayList<coordinateObject> history = new ArrayList<>();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ElapsedTime etime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
+        while (opModeIsActive()) {
+            telemetry.addData("time right now = ", etime.now(TimeUnit.MILLISECONDS));
+            telemetry.update();
+            if(etime.now(TimeUnit.MILLISECONDS) % 10 == 0)
+            {
+
+            }
+        }
+    }
+
+
 
     //adds new coordinateObject objects to the arrayList
     public void newCoord (double x, double y)
@@ -24,5 +44,7 @@ public class coordinateMapMaker
     {
         return history.toString();
     }
+
+
 
 }
